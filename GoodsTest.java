901cd01ab2ce2c017c8d90f9200caaa700f3dc8b
YANG JIHYEON <83569982+YSoori@users.noreply.github.com>
2021-09-14 13:48:34 +0900

    Update GoodsTest.java
    등록한 재고 정보를 메모장에 기록해서 다시 실행하더라도 전에 저장했던 정보를 불러 올 수 있게 변경

--- GoodsTest.java
+++ GoodsTest.java
@@ -1,17 +1,13 @@
-import java.util.*;
+package C0914_IO_PJT;
 
-import javax.sound.sampled.Line;
+import java.util.*;
+import java.io.*;
 
 public class GoodsTest {
 	
-	//재고 배열의 인덱스를 나타내는 전역변수
-	public static int cnt = 0;
-	
 	//화면을 나누는 메소드
 	static void line() {
-		System.out.println();
-		System.out.println("=========================================");
-		System.out.println();
+		System.out.println("\n=========================================\n");
 	}
 	
 	//해당 재고가 이미 존재하는지 확인하는 메소드
@@ -53,31 +49,80 @@ public class GoodsTest {
 		cnt--;
 	}
 	
+	//재고 배열의 인덱스를 나타내는 전역변수
+	public static int cnt = 0;
+	
+	// 우선 재고가 들어갈 배열생성
+	static GoodsInfo[] g = new GoodsInfo[100]; 
+	
+	static boolean readFile(String path) {
+		LineNumberReader reader = null;
+		try {
+			reader = new LineNumberReader(new FileReader(path));
+			while(true) {
+				String str = reader.readLine();
+				if(str==null) {
+					break;
+				}
+				String[] result = str.split(" ");
+				g[cnt++] = new GoodsInfo(result[0], Integer.parseInt(result[1]));
+			}
+			
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("파일이 존재하지 않습니다.");
+		} catch(IOException ioe){
+			System.out.println("파일을 읽을 수 없습니다.");
+		}finally {
+		
+			try {
+				reader.close();
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+	}
+	
+	static boolean writeFile(String path) {
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(path);
+			for(int i=0;i<cnt;i++) {
+				writer.write(g[i].code +" "+g[i].stocks+"\n");
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("파일을 출력할 수 없습니다.");
+		} finally {
+			try {
+				writer.close();
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		String code; // 상품명
 		int stocks, select=0, delCnt; //수량, 프로그램 조작 버튼, 삭제할 재고의 인덱스
-		
-		GoodsInfo[] g = new GoodsInfo[100]; // 우선 재고가 들어갈 배열생성
-		
 		GoodsInfo g2 = null; //입고 시 중복이 있는지 확인할 변수
 		
 		System.out.println("재고 관리 프로그램을 실행합니다.");
+		readFile("C0914_IO_PJT/stocks.txt");
 		line();
 		
 		//반복문 사용하여 반복문이 종료될 때까지 계속 실행.
 		while(select!=5) {
 			
 			//조작에 사용될 번호들의 정보 출력
-			System.out.println("1.입고 2.출고 3.삭제 4.현황 5.종료");
-			
-			line();
+			System.out.print("1.입고 2.출고 3.삭제 4.현황 5.종료 : ");
 			
 			//조작할 번호를 스캐너를 통해 입력받는다.
 			select = sc.nextInt();
-			System.out.println();
 			
+			line();
 			
 			//입력받은 번호를 스위치문을 사용해서 조작한다.
 			switch (select) {
@@ -90,9 +135,12 @@ public class GoodsTest {
 				//이미 등록된 상품이 존재하면 해당 상품의 수량만 입력받은 수량만큼 증가시킨다.
 				if(g2==null) {
 					g[cnt++] = new GoodsInfo(code, stocks);
+					
 				}else {
 					g2.store(stocks);
+					
 				}
+				writeFile("C0914_IO_PJT/stocks.txt");
 				
 				line();
 				break;
@@ -117,6 +165,7 @@ public class GoodsTest {
 					}
 					//해당하는 재고가 존재하면 g2.out(stocks)메소드를 수행. 
 				}
+				writeFile("C0914_IO_PJT/stocks.txt");
 				line();
 				break;
 				
@@ -138,14 +187,16 @@ public class GoodsTest {
 					}
 					//해당하는 재고가 존재하면 delGoods(g, delCnt)메소드를 수행. 
 				}
+				writeFile("C0914_IO_PJT/stocks.txt");
 				line();
 				break;
 				
 			//4번 입력시 등록된 재고의 이름과 수량을 모두 보여줌.	
 			case 4:
 				System.out.println("재고 현황을 출력합니다.");
-				line();
+				
 				showAll(g);
+				line();
 				break;
 				
 			//5번 입력시 프로그램을 종료하고 반복문을 빠져나감.	
